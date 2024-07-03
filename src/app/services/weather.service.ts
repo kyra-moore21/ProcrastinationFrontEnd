b4d68e80d84a87e1b8878823d6e2bdfe9d881454
kyra-moore21 <kyra.moore21@icloud.com>
2024-07-03 12:26:49 -0400

    added weather get method

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherModel } from '../models/weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getWeather():Observable<WeatherModel>{
+    return this.http.get<WeatherModel>(`https://api.openweathermap.org/data/2.5/weather?q=Hazel Park&appid=6b25bb910b9643ca68b3e0053261f2b2&units=imperial"`);
+  }
 }
