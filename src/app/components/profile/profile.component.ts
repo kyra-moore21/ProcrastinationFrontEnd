6d22b095d857a130e9a6374e1c02e07fbacdf7a2
Ramsey Louder <RamseyLouder@gmail.com>
2024-07-10 11:24:04 -0400

    profile component, html

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { UserModel } from '../../models/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+constructor(private userService: UserService,){}
+currentUsers: UserModel  = {}as UserModel;
+
+ngOnInit(){
+  this.getCurrentUser();
+ 
+}
+
+getCurrentUser(): UserModel{
+  return this.currentUsers = this.userService.currentUser;
+}
+
+updateUser(targetUser: UserModel){
+this.userService.UpdateUser(targetUser).subscribe((response)=>{
+  console.log(response);
+  this.getCurrentUser();
+})
+}
 
 }
