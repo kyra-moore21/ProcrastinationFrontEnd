73d2ce7afc1d5c1395c7423b87f91b15f398dd7d
kyra-moore21 <kyra.moore21@icloud.com>
2024-09-18 17:23:14 -0400

    ran ng build for prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = { production: true,
-    clientId: "539781147741-fvg3mj7qu5o2h699rhllua905pe33m66.apps.googleusercontent.com",
-    url: "https://procrastinatorbackend20240705110551.azurewebsites.net"
+    clientId: "637203135435-dftfe2k50dgfkoshfr92hf81t91dkbgt.apps.googleusercontent.com",
+    url: "https://procrastinatorbackend-dev.eba-bviev6t3.us-east-1.elasticbeanstalk.com"
 };
