95cfadf4d7c2eafc10f06dac32b1a2d3547d9514
kyra-moore21 <kyra.moore21@icloud.com>
2024-09-18 21:44:43 -0400

    fixed google login and updated api url again

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,35 +1,35 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 import { environment } from '../environments/environment.development';
-import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
 import { DatePipe } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-     //We are building a provider. Version 2.2.0 of package doesn't provide one.
-     {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
       provide: 'SocialAuthServiceConfig',
       useValue: {
         autoLogin: false,
         providers: [
           {
             id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              //Remove the .apps.googleusercontent.com from the client id
-              //MAKE SURE TO HIDE IT FROM GITHUB
-              environment.clientId
-            ),
-          },
+            provider: new GoogleLoginProvider(environment.clientId, {
+              oneTapEnabled: false, // Disable One Tap as it's not compatible with FedCM
+              scopes: 'firstname lastname photourl email', // Adjust scopes as needed
+              prompt_parent_id: 'google-btn', // ID of the element where the button will be rendered
+            }),
+          }
         ],
         onError: (err) => {
-          debugger;
           console.error(err);
         },
       } as SocialAuthServiceConfig,
     },
-    //more providers can go here
-   provideHttpClient(), DatePipe]};
+    provideHttpClient(),
+    DatePipe
+  ]
+};
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = { production: true,
     clientId: "637203135435-dftfe2k50dgfkoshfr92hf81t91dkbgt.apps.googleusercontent.com",
-    url: "https://procrastinatorbackend-dev.eba-cdkwtivj.us-east-2.elasticbeanstalk.com/"
+    url: "http://procrastinatorbackend-dev.eba-cdkwtivj.us-east-2.elasticbeanstalk.com"
 };
